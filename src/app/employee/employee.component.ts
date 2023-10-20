5061a9c1f5a08fd1f4a7a971d829f63b94e155d7
ajmris <szymektarasiewicz@onet.pl>
2023-10-20 18:33:20 +0200

    two-way binding

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -10,10 +10,4 @@ export class EmployeeComponent {
   skill='Angular';
   constructor(){
   }
-  inputProfession(event: Event){
-    this.profession=(event.target as HTMLInputElement).value;
-  }
-  inputSkill(event: Event){
-    this.skill=(event.target as HTMLInputElement).value;
-  }
 }
